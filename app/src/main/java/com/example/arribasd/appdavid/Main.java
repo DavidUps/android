324f07be5f2417110827ce9b2d7018f4ef5e54d8
DavidUps <davidarribas.ciclos@gmail.com>
2017-12-21 18:15:23 +0100

    App

--- app/src/main/java/com/example/arribasd/appdavid/Main.java
+++ app/src/main/java/com/example/arribasd/appdavid/Main.java
@@ -1,18 +1,21 @@
 package com.example.arribasd.appdavid;
 
-import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.example.arribasd.appdavid.Adapter.Lista;
+import com.example.arribasd.appdavid.Fragments.ListFragment;
+import com.example.arribasd.appdavid.Fragments.ProfileFragment;
 
 import java.util.ArrayList;
 
 public class Main extends AppCompatActivity {
 
-    public static ArrayList<String> lista = new ArrayList<>();
+    public static ArrayList<Lista> lista = new ArrayList<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -55,10 +58,12 @@ public class Main extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        ListFragment fragmentList = new ListFragment();
-        android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction2.replace(R.id.fragmentMain, fragmentList, "Fragment Name");
-        fragmentTransaction2.commit();
+        ProfileFragment fragment = new ProfileFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragmentMain, fragment, "Fragment name");
+        fragmentTransaction.commit();
+
+        lista.add(new Lista("david","dadad", Uri.EMPTY));
 
     }
 
