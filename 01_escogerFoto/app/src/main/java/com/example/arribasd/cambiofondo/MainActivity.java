b93c58b0ae0aa9f4604d06bb551a5b0cbfbacf58
DavidUps <15121996david>
2018-01-10 12:22:34 +0100

    Y TE LA ENVIA POR CORREO!!

--- 01_escogerFoto/app/src/main/java/com/example/arribasd/cambiofondo/MainActivity.java
+++ 01_escogerFoto/app/src/main/java/com/example/arribasd/cambiofondo/MainActivity.java
@@ -16,7 +16,11 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView = null;
+    Button button = null;
+    private Intent chosser;
     private final int REQUEST_CODE_IMAGE = 1;
+    private final int REQUEST_CODE_EMAIL = 2;
+    Uri uri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         imageView = (ImageView) findViewById(R.id.selectImg);
-        Button button = (Button) findViewById(R.id.saveBtn);
+        button = (Button) findViewById(R.id.saveBtn);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,15 +41,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(intent.EXTRA_EMAIL,"Gabriel.ArroyoPajares@unisys.com");
+                intent.putExtra(intent.EXTRA_SUBJECT, "Hola Gabi XDDD: ");
+                intent.putExtra(Intent.EXTRA_TEXT, "Te envio esta imagen desde mi app del movil, como ola la mola");
+                intent.setType("message/rfc822");
+                chosser = Intent.createChooser(intent, "Enviar Email");
+                intent.putExtra(intent.EXTRA_STREAM, uri);
+                startActivity(chosser);
+
+            }
+        });
+
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(resultCode == RESULT_OK){
             if(requestCode == REQUEST_CODE_IMAGE){
-                Uri uri = data.getData();
+                uri = data.getData();
                 imageView.setImageURI(uri);
             }else{
                 Toast.makeText(MainActivity.this,"Compruebe la imagen", Toast.LENGTH_LONG).show();
+            }
+            if(requestCode == REQUEST_CODE_EMAIL){
+                Toast.makeText(MainActivity.this, "YEII mensage enviado", Toast.LENGTH_LONG).show();
             }
         }
     }
