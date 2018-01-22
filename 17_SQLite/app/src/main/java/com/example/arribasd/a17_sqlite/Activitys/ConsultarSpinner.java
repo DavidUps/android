037c0212944f2b565985432081fba0e3a944ee50
DavidUps <15121996david>
2018-01-22 13:01:12 +0100

    SQLite

---
+++ 17_SQLite/app/src/main/java/com/example/arribasd/a17_sqlite/Activitys/ConsultarSpinner.java
@@ -0,0 +1,15 @@
+package com.example.arribasd.a17_sqlite.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.arribasd.a17_sqlite.R;
+
+public class ConsultarSpinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_spinner);
+    }
+}
